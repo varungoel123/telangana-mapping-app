8ed978ecb8d8f6826d9719264e71600bd88de0d4
Varun Goel <varungoel123@gmail.com>
2015-09-21 16:21:03 +0530

    changed filenames

--- map_files/data/telangana_csvToPostgres.R
+++ map_files/data/telangana_csvToPostgres.R
@@ -3,12 +3,9 @@ library(dplyr)
 library(magrittr)
 library(stringr)
 
-setwd("~/Documents/ResRA/main_app_v1/data/")
+#setwd("~/Documents/ResRA/main_app_v1/data/")
 source("./db_details.R") # contains confidential db details
-pw <- "resra@!$3"
-dbname = "appdev_db"
-host = "139.162.17.147"
-username = "postgres"
+
 # loads the PostgreSQL driver
 drv <- dbDriver("PostgreSQL")
 # creates a connection to the postgres database
@@ -21,14 +18,16 @@ con <- dbConnect(drv, dbname = dbname,
                  host = host, port = 5432,
                  user = username, password = pw)
 
-file_list <- "hierarchy_lookup_agcensus_05_crop_district.csv"
+file_list <- dir("./data",pattern = ".csv$")[-1]
+#file_list = "variables_telangana_rkvy_12_14_district.csv" 
 for (i in seq(file_list)){
 dbWriteTable(con, str_replace_all(file_list[i],".csv",""),
              assign(str_replace_all(file_list[i],".csv",""),
-                    read.csv(file_list[i])))
+                    read.csv(paste("./data",file_list[i],sep="/"))))
 }
 
 #### drop all tables in db with DROP SCHEMA public cascade;
 ### then use CREATE SCHEMA public;
+### SHORTEN SOME NAMES: EG hierarchy_lookup_telangana_fertilizer_dealers_till_2013_district
 
 
